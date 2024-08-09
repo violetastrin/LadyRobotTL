5ace25a178e2742e3ae40ca1b04605fc789452f1
Violeta Trindade <mvioleta.st@gmail.com>
2024-08-09 09:36:19 -0300

    Add files via upload

---
+++ Testes Sensores/testar_sensores.ino
@@ -0,0 +1,31 @@
+#define sensor1 A1
+#define sensor2 A2
+#define sensor3 A3
+#define sensor4 A4
+#define sensor5 A0
+
+void setup() {
+
+  Serial.begin(9600);                    // Inicial Serial em 9600 Baud Rate.
+  Serial.println("Serial Trabalhando");  // Teste para verificar se serial está funcionando ou não
+
+  pinMode(sensor1, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
+  pinMode(sensor5, INPUT);
+}
+
+void loop() {
+  //-----------------------------------------Testando os Sensores--------------------------------------------------------
+  Serial.print(" sensor1 = ");
+  Serial.println(analogRead(sensor5));
+  Serial.print(" sensor2 = ");
+  Serial.print(analogRead(sensor1));
+  Serial.print(" sensor3 = ");
+  Serial.print(analogRead(sensor2));
+  Serial.print(" sensor4 = ");
+  Serial.print(analogRead(sensor4));
+  Serial.print(" sensor5 = ");
+  Serial.print(analogRead(sensor3));
+}
