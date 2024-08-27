975efcd41d4478eb8fca946d8a18b2f9607a8bc5
Violeta Trindade <mvioleta.st@gmail.com>
2024-08-27 11:21:34 -0300

    Update TCS3472teste.ino

--- Testes Sensores/TCS3472teste.ino
+++ Testes Sensores/TCS3472teste.ino
@@ -2,10 +2,10 @@
 #include <Wire.h>
 #include <Adafruit_TCS34725_SWwire.h>
 
-SoftwareWire sWire(A2, A3);
+SoftwareWire sWire(A6, A7);
 
-Adafruit_TCS34725_SWwire tcs_real = Adafruit_TCS34725_SWwire(TCS34725_INTEGRATIONTIME_499MS, TCS34725_GAIN_60X);
-Adafruit_TCS34725_SWwire tcs_soft = Adafruit_TCS34725_SWwire(TCS34725_INTEGRATIONTIME_499MS, TCS34725_GAIN_60X);
+Adafruit_TCS34725_SWwire tcs_real = Adafruit_TCS34725_SWwire(TCS34725_INTEGRATIONTIME_180MS, TCS34725_GAIN_16X);
+Adafruit_TCS34725_SWwire tcs_soft = Adafruit_TCS34725_SWwire(TCS34725_INTEGRATIONTIME_180MS, TCS34725_GAIN_16X);
 
 void setup(void) { 
   Serial.begin(9600);
@@ -34,6 +34,14 @@ void loop(void) {
   tcs_real.getRawData(&r1, &g1, &b1, &c1);
   tcs_soft.getRawData(&r2, &g2, &b2, &c2);
 
+  uint16_t t1  = (r1+g1+b1)/3;
+  uint16_t tr1 = (g1+b1)/2;
+  uint16_t tg1 = (r1+b1)/2;
+
+  uint16_t t2  = (r2+g2+b2)/3;
+  uint16_t tr2 = (g2+b2)/2;
+  uint16_t tg2 = (r2+b2)/2;
+
   Serial.print("ESQ (soft): ");
   Serial.print("Vermelho: "); Serial.print(r2);
   Serial.print(", Verde: "); Serial.print(g2);
@@ -45,45 +53,4 @@ void loop(void) {
   Serial.print(", Verde: "); Serial.print(g1);
   Serial.print(", Azul: "); Serial.print(b1);
   Serial.print(", Claro: "); Serial.println(c1);
-
-
-  uint16_t medgeral1  = (r1+g1+b1)/3;
-  uint16_t medgb1 = (g1+b1)/2;
-  uint16_t medrb1 = (r1+b1)/2;
-
-  uint16_t medgeral2  = (r2+g2+b2)/3;
-  uint16_t medgb2 = (g2+b2)/2;
-  uint16_t medrb2 = (r2+b2)/2;
-
-  Serial.print("direita(real): ");
-  if(r1 > medgb1)
-    Serial.print("vermelho");
-  else if(g1 > medrb1)
-    Serial.print("verde");
-  else {
-    if(c1 >= 11500) //1050
-      Serial.print("branco");
-    else 
-      Serial.print("preto");
-  }
-
-  Serial.print(" / ");
-
-  Serial.print("esquerda(soft): ");
-  if(r2 > medgb2)
-    Serial.print("vermelho");
-  else if(g2 > medrb2)
-    Serial.print("verde");
-  else {
-    if(medgeral2 >= 11500) //1050
-      Serial.print("branco");
-    else 
-      Serial.print("preto");
-  }
-
-  Serial.println("");
-
-
-
-
 }
