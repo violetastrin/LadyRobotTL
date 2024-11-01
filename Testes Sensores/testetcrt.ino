852bdb42d26172e080a1ee0170ec46033367e778
torquArt <157934500+torquArt@users.noreply.github.com>
2024-11-01 13:03:33 -0300

    pinos certos

--- Testes Sensores/testetcrt.ino
+++ Testes Sensores/testetcrt.ino
@@ -1,31 +1,27 @@
-#define sensor1 A1           // direita
-#define sensor2 A2          // r dir queimado capenga 
-#define sensor3 A3          //r esq 
-#define sensor4 A4          // frente
-#define sensor5 A0           // esquerda
+
+int sensor[] = {A1, A0, A4, A2, A3};
+// ORDEM: ESQUERDA, REAJUSTE ESQUERDA, FRENTE, REAJUSTE DIREITA, DIREITA
 
 void setup() {
 
   Serial.begin(9600);                    // Inicial Serial em 9600 Baud Rate.
   Serial.println("Serial Trabalhando");  // Teste para verificar se serial está funcionando ou não
 
-  pinMode(sensor1, INPUT);
-  pinMode(sensor2, INPUT);
-  pinMode(sensor3, INPUT);
-  pinMode(sensor4, INPUT);
-  pinMode(sensor5, INPUT);
+for(int i = 0; i>5; i++){
+  pinMode(sensor[i], INPUT);
+}
 }
 
 void loop() {
   //-----------------------------------------Testando os Sensores--------------------------------------------------------
   Serial.print(" sensor1 = ");
-  Serial.println(analogRead(sensor5));
+  Serial.println(analogRead(sensor[0]));
   Serial.print(" sensor2 = ");
-  Serial.print(analogRead(sensor1));
+  Serial.print(analogRead(sensor[1]));
   Serial.print(" sensor3 = ");
-  Serial.print(analogRead(sensor2));
+  Serial.print(analogRead(sensor[2]));
   Serial.print(" sensor4 = ");
-  Serial.print(analogRead(sensor4));
+  Serial.print(analogRead(sensor[3]));
   Serial.print(" sensor5 = ");
-  Serial.print(analogRead(sensor3));
+  Serial.print(analogRead(sensor[4]));
 }
